4e0930eb8b1ca75141f44aaf73d988a8264922e8
Nathan Baggs <mailme@nbaggs.com>
2013-12-14 11:11:25 +0000

    added player controls and ground texture.

--- includes/Game.hpp
+++ includes/Game.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 
 #include "FluxRoot.hpp"
+#include "FluxTextureMaterial.hpp"
+
+class Player;
 
 class Game
 {
@@ -22,6 +25,16 @@ class Game
         Flux::Root *root;
         bool alive = true;
         SDL_Event event;
+
+        std::shared_ptr<Player> player;
+        std::shared_ptr<Flux::SpriteBatch> groundSB;
+        std::shared_ptr<Flux::Sprite> ground;
+
+        bool keyA = false;
+        bool keyD = false;
+        bool keyW = false;
+        bool keyS = false;
+        Flux::Vector2 tmpVelocity = Flux::Vector2::ZERO;
 };
 
 #endif
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.hpp"
+#include "Player.hpp"
 
 Game::Game()
     : root(new Flux::Root())
@@ -12,11 +13,24 @@ Game::~Game()
 void Game::initialise()
 {
     root->initialise();
+
+    player = std::make_shared<Player>(root);
+
+    auto texture = root->getTextureManager()->loadTexture("assets/ground.png");
+    auto mat = std::make_shared<Flux::TextureMaterial>(texture);
+    groundSB = std::make_shared<Flux::SpriteBatch>(mat);
+
+    ground = std::make_shared<Flux::Sprite>(Flux::Rectangle2D(0.0f, 0.0f, 1024.0f, 1024.0f), Flux::Colour(1.0f, 1.0f, 1.0f, 0.0f), Flux::Rectangle2D(0.0f, 0.0f, 1024.0f, 1024.0f));
+    groundSB->addSprite(ground);
+    ground->setDepth(-5.0f);
+    root->addToScene(groundSB);
 }
 
 void Game::tick()
 {
     handleInput();
+    player->setVelocity(Flux::Vector2::normalise(tmpVelocity) * player->getSpeed());
+    player->update();
     root->renderFrame();
 }
 
@@ -45,12 +59,75 @@ void Game::handleInput()
                         alive = false;
                         break;
                     }
+                    case SDLK_a :
+                    {
+                        tmpVelocity.x = -1.0f;
+                        keyA = true;
+                        break;
+                    }
+                    case SDLK_d :
+                    {
+                        tmpVelocity.x = 1.0f;
+                        keyD = true;
+                        break;
+                    }
+                    case SDLK_w :
+                    {
+                        tmpVelocity.y = 1.0f;
+                        keyW = true;
+                        break;
+                    }
+                    case SDLK_s :
+                    {
+                        tmpVelocity.y = -1.0f;
+                        break;
+                    }
                     default : break;
                 }
                 break;
             }
             case SDL_KEYUP :
             {
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_a :
+                    {
+                        keyA = false;
+                        if(!keyD)
+                            tmpVelocity.x = 0.0f;
+                        else
+                            tmpVelocity.x = 1.0f;
+                        break;
+                    }
+                    case SDLK_d :
+                    {
+                        keyD = false;
+                        if(!keyA)
+                            tmpVelocity.x = 0.0f;
+                        else
+                            tmpVelocity.x = -1.0f;
+                        break;
+                    }
+                    case SDLK_w :
+                    {
+                        keyW = false;
+                        if(!keyS)
+                            tmpVelocity.y = 0.0f;
+                        else
+                            tmpVelocity.y = -1.0f;
+                        break;
+                    }
+                    case SDLK_s :
+                    {
+                        keyS = false;
+                        if(!keyW)
+                            tmpVelocity.y = 0.0f;
+                        else
+                            tmpVelocity.y = 1.0f;
+                        break;
+                    }
+                    default : break;
+                }
                 break;
             }
             
