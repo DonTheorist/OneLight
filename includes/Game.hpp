01212ad8fa3b2569d3ca702ebc7706f45ac378b3
Nathan Baggs <mailme@nbaggs.com>
2013-12-15 08:21:03 +0000

    created a basic State manager (stack) and moved all game logic to GameState class.

--- includes/Game.hpp
+++ includes/Game.hpp
@@ -2,15 +2,14 @@
 #define ONELIGHT_GAME_H
 
 #include <iostream>
+#include <stack>
 #include <vector>
 #include <math.h>
 
 #include "FluxRoot.hpp"
 #include "FluxNormalMaterial.hpp"
 
-class Player;
-class OilLamp;
-class LevelManager;
+#include "GameState.hpp"
 
 class Game
 {
@@ -23,24 +22,14 @@ class Game
         void initialise();
         void tick();
         bool isAlive();
-        void handleInput();
+        void setAlive(const bool alive);
 
     private:
         Flux::Root *root;
         bool alive = true;
-        SDL_Event event;
 
-        std::shared_ptr<Player> player;
-        std::shared_ptr<OilLamp> lamp;
-        std::shared_ptr<Flux::SpriteBatch> groundSB;
-        std::shared_ptr<Flux::Sprite> ground;
-        std::shared_ptr<LevelManager> levelManager;
+        std::stack<State*> states;
 
-        bool keyA = false;
-        bool keyD = false;
-        bool keyW = false;
-        bool keyS = false;
-        Flux::Vector2 tmpVelocity = Flux::Vector2::ZERO;
 };
 
 #endif
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,4 @@
 #include "Game.hpp"
-#include "Player.hpp"
-#include "OilLamp.hpp"
 #include "Block.hpp"
 #include "LevelManager.hpp"
 
@@ -17,33 +15,14 @@ void Game::initialise()
 {
     root->initialise();
 
-    player = std::make_shared<Player>(root);
-
-    auto texture = root->getTextureManager()->loadTexture("assets/ground.png");
-    auto normal = root->getTextureManager()->loadTexture("assets/groundNormal.png");
-    auto mat = std::make_shared<Flux::NormalMaterial>(texture, normal);
-    groundSB = std::make_shared<Flux::SpriteBatch>(mat);
-    groundSB->setLightEnabled(true);
-
-    ground = std::make_shared<Flux::Sprite>(Flux::Rectangle2D(0.0f, 0.0f, 1024.0f, 1024.0f), Flux::Colour::WHITE, Flux::Rectangle2D(0.0f, 0.0f, 1024.0f, 1024.0f), Flux::Rectangle2D(0.0f, 0.0f, 1024.0f, 1024.0f));
-    groundSB->addSprite(ground);
-    ground->setDepth(-1.0f);
-    root->addToScene(groundSB);
-
-    levelManager = std::make_shared<LevelManager>(player, root);
-    levelManager->loadLevel(0);
-
-    lamp = std::make_shared<OilLamp>(player, root);
+    GameState *gameState = new GameState(this);
+    gameState->initialise();
+    states.push(gameState);
 }
 
 void Game::tick()
 {
-    handleInput();
-
-    player->setVelocity(Flux::Vector2::normalise(tmpVelocity) * player->getSpeed());
-    player->update();
-    lamp->update();
-    levelManager->update();
+    states.top()->update();
 
     root->renderFrame();
 }
@@ -58,112 +37,8 @@ bool Game::isAlive()
     return alive;
 }
 
-void Game::handleInput()
+void Game::setAlive(const bool alive)
 {
-    while(SDL_PollEvent(&event))
-    {
-        switch(event.type)
-        {
-            case SDL_KEYDOWN :
-            {
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_ESCAPE :
-                    {
-                        alive = false;
-                        break;
-                    }
-                    case SDLK_a :
-                    {
-                        tmpVelocity.x = -1.0f;
-                        keyA = true;
-                        break;
-                    }
-                    case SDLK_d :
-                    {
-                        tmpVelocity.x = 1.0f;
-                        keyD = true;
-                        break;
-                    }
-                    case SDLK_w :
-                    {
-                        tmpVelocity.y = 1.0f;
-                        keyW = true;
-                        break;
-                    }
-                    case SDLK_s :
-                    {
-                        tmpVelocity.y = -1.0f;
-                        break;
-                    }
-                    case SDLK_q :
-                    {
-                        //lightIncreasePercentage = -lightIncreaseRate;
-                        lamp->decreaseLightPercentgae();
-                        break;
-                    }
-                    case SDLK_e :
-                    {
-                        lamp->increaseLightPercentage();
-                        //lightIncreasePercentage = lightIncreaseRate;
-                        break;
-                    }
-                    default : break;
-                }
-                break;
-            }
-            case SDL_KEYUP :
-            {
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_a :
-                    {
-                        keyA = false;
-                        if(!keyD)
-                            tmpVelocity.x = 0.0f;
-                        else
-                            tmpVelocity.x = 1.0f;
-                        break;
-                    }
-                    case SDLK_d :
-                    {
-                        keyD = false;
-                        if(!keyA)
-                            tmpVelocity.x = 0.0f;
-                        else
-                            tmpVelocity.x = -1.0f;
-                        break;
-                    }
-                    case SDLK_w :
-                    {
-                        keyW = false;
-                        if(!keyS)
-                            tmpVelocity.y = 0.0f;
-                        else
-                            tmpVelocity.y = -1.0f;
-                        break;
-                    }
-                    case SDLK_s :
-                    {
-                        keyS = false;
-                        if(!keyW)
-                            tmpVelocity.y = 0.0f;
-                        else
-                            tmpVelocity.y = 1.0f;
-                        break;
-                    }
-                    case SDLK_q : case SDLK_e :
-                    {
-                        lamp->resetLightPercentage();
-                        break;
-                    }
-                    default : break;
-                }
-                break;
-            }
-            
-            default : break;
-        }
-    }
+    this->alive = alive;
 }
 
